42ad57e37d42e442921c58ae754a217013db2981
JuliaSelyutina <JSSelyutina@yandex.ru>
2022-12-30 01:59:01 +0300

    6.1. Генерация конструкций в IDEA [#279134].

---
+++ src/main/java/ru/job4j/live/Main.java
@@ -0,0 +1,7 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Generate with psvm + TAB - main, then with sout + TAB - System.out.println, then witn fori + TAB - loop for i");
+    }
+}
