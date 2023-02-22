6f80cef694504fb5dd05a9b77c7396f5c22f93cc
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-02-23 00:55:37 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587].

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,60 +8,50 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double delta = 0.01;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, delta);
     }
 
     @Test
     public void when34to08then5() {
         double expected = 5;
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 0;
-        int y2 = 8;
+        Point a = new Point(3, 4);
+        Point b = new Point(0, 8);
         double delta = 0.01;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, delta);
     }
 
     @Test
     public void when17to1030then24dot7() {
         double expected = 24.7;
-        int x1 = 1;
-        int y1 = 7;
-        int x2 = 10;
-        int y2 = 30;
+        Point a = new Point(1, 7);
+        Point b = new Point(10, 30);
         double delta = 0.01;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, delta);
     }
 
     @Test
     public void when1325to1827then5dot39() {
         double expected = 5.39;
-        int x1 = 13;
-        int y1 = 25;
-        int x2 = 18;
-        int y2 = 27;
+        Point a = new Point(13, 25);
+        Point b = new Point(18, 27);
         double delta = 0.01;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, delta);
     }
 
     @Test
     public void when66to54then2dot24() {
         double expected = 2.24;
-        int x1 = 6;
-        int y1 = 6;
-        int x2 = 5;
-        int y2 = 4;
+        Point a = new Point(6, 6);
+        Point b = new Point(5, 4);
         double delta = 0.01;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, delta);
     }
 }
